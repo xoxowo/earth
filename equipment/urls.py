303451a3cc1711c0823310be184fc41b35a80a1c
melony <hjz1010@gmail.com>
2022-08-30 19:44:36 +0900

    중장비 통계 페이지 가동률 표출 API

--- equipment/urls.py
+++ equipment/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from equipment.views import EquipmonetListView, EquipmentDetailView
+from equipment.views import EquipmonetListView, EquipmentDetailView, AnalysisView
 
 urlpatterns = [
    path('/list', EquipmonetListView.as_view()),
    path('/<int:equipment_id>', EquipmentDetailView.as_view()),
+   path('/analysis', AnalysisView.as_view()),
+
 ]
