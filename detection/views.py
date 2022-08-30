06b56af9f18cd17190838a90e41f96509d2b1618
xoxowo <1qq11212@gmail.com>
2022-08-30 16:26:14 +0900

    realtimeview 기능 구현 완료

--- detection/views.py
+++ detection/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+import datetime
 
-# Create your views here.
+from django.http       import JsonResponse
+from django.utils      import timezone
+from django.views      import View
+
+from detection.models import Detection
+
+class RealTimeView(View):
+    def get(self, request):
+        try:
+            detection_options = Detection.objects.filter(datetime__gte = timezone.now() - datetime.timedelta(seconds=10))
+            real_time         = detection_options.first()
+
+            if real_time == None :
+                return JsonResponse({'message':'Not_Detected'}, status=400)
+            
+            results = [{
+                'datetime'  : real_time.datetime,
+                'type' : [{ 
+                    'detection_info': detection_option.detection_type.name,
+                    'state'         : detection_option.state.equipment_state,
+                    } for detection_option in detection_options]
+            }]
+            
+            return JsonResponse({'message':results}, status=200)
+
+        except KeyError:
+            JsonResponse({'message':'Key_Error'}, status=400)
