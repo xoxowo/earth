fb39bf6bbead5a95be497c8f00c22b6612b8a74c
melony <hjz1010@gmail.com>
2022-08-31 14:56:53 +0900

    - 불필요한 주석 제거 - 하드코딩 하나 더 수정(PROGRESS_PER_ONE)

--- mqtt_subscribe.py
+++ mqtt_subscribe.py
@@ -30,8 +30,7 @@ def on_message(client, userdata, msg):
     datetime               = data['datetime']   # "2022-08-18T11:45:41+0900"
     detection_informations = data['detection_information']
       
-    # YYYY-MM-DD hh:mm:ss 형태로 변형 필요  ## fromisoformat은 형태가 약간 안 맞음 ㅠㅠ
-    datetime_split = re.split('[T|+]',datetime)   # T 랑 + 기준으로 한번에 split 하는 방법!
+    datetime_split = re.split('[T|+]',datetime) 
     datetime1      = datetime_split[0] + ' ' + datetime_split[1]
 
     for i in range(detection_count):
@@ -45,16 +44,15 @@ def on_message(client, userdata, msg):
         height         = detection_information['height']
         state          = detection_information['state']     
  
-        # detection_type1 = DetectionType.objects.get(name = detection_type) 이걸 SQL문으로..!
+
         mycursor.execute('SELECT id FROM detection_types WHERE name=%s', (detection_type,))
         detection_type1, = mycursor.fetchone()  
-        # print(detection_type1)  # fetchone()해서 찍어보니 (1,)로 나와서 detection_type뒤에 ,찍어서 값만 할당!
 
-        # state1 = State.objects.get(name = state)
         mycursor.execute('SELECT id FROM states WHERE equipment_state=%s', (state,))
         state1, = mycursor.fetchone()
-        # print(state1)
+
         print(serial_number, end=', ')
+
         if detection_type == 'truck':
             equipment1 = None
         else :
@@ -62,16 +60,19 @@ def on_message(client, userdata, msg):
             equipment1, = mycursor.fetchone()
 
         PROGRESS_DETECTION = 'wheel_loader-000' # 이걸로 가정
-        START_POINT = 0  # 시작 x좌표는 0으로 가정
-        TURNING_POINT = 2000  # 도착 x좌표는 2000으로 가정
+        START_POINT        = 0                  # 시작 x좌표는 0으로 가정
+        TURNING_POINT      = 2000               # 도착 x좌표는 2000으로 가정
+        PROGRESS_PER_ONE   = 10                 # 한바퀴 당 공정률
 
         if serial_number == PROGRESS_DETECTION:  
             last          = Detection.objects.filter(serial_number=serial_number).last()
             last_x        = last.x        if last else START_POINT
             last_progress = last.progress if last and last.progress else 0
 
-            if last_x < x <= TURNING_POINT :    # 음... 2000에 멈춰있으면..... last_x = x 이므로 false!!
-                progress = last_progress//10 *10 + x/TURNING_POINT*10
+            if last_x < x <= TURNING_POINT :  
+                rounds   = last_progress//PROGRESS_PER_ONE
+                progress = rounds * PROGRESS_PER_ONE \
+                         + x/TURNING_POINT * PROGRESS_PER_ONE
             else: 
                 progress = last_progress
         else :
@@ -84,15 +85,15 @@ def on_message(client, userdata, msg):
         val = (x,y,width,height,serial_number,datetime1,cam_id, detection_type1, state1, equipment1, progress)
         mycursor.execute(sql,val)
 
-        mydb.commit() # commit을 해줘야 DB에 실제로 입력이 됨
+        mydb.commit()
            
     print('\n >>> %s %s개의 데이터를 저장했습니다.' % (datetime1, detection_count))
 
-client = mqtt.Client(client_id="") # client_id는 생략 가능
+client = mqtt.Client(client_id="")
 client.on_connect = on_connect
-client.on_message = on_message  # subscribe 메세지가 들어오면 작동하는 Method
+client.on_message = on_message  
 
-ca_file, cert_file, key_file = './certification/ca.pem', './certification/cert.pem', './certification/cert-key.pem' # 같이 드린 파일
+ca_file, cert_file, key_file = './certification/ca.pem', './certification/cert.pem', './certification/cert-key.pem' 
 
 mqtt_endpoint = 'mqtt-dsmepbs.cauto.musma.net'
 client.tls_set(ca_certs=ca_file, certfile=cert_file, keyfile=key_file, cert_reqs=ssl.CERT_REQUIRED, tls_version=ssl.PROTOCOL_TLSv1_2, ciphers=None)
