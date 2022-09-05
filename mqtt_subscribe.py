d73fecbdd5d7466d8e988b05e004129b291a9af2
melony <hjz1010@gmail.com>
2022-09-05 13:35:41 +0900

    mqtt_subscribe 주석추가

--- mqtt_subscribe.py
+++ mqtt_subscribe.py
@@ -10,15 +10,15 @@ from core.utils import save_data_in_MYSQL
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code", rc)
 
-data = None
+data = None # message로 받아온 데이터를 저장해둘 변수
 def on_message(client, userdata, msg):
     global data
     data = json.loads(msg.payload.decode('utf-8'))
     save_data_in_MYSQL(data)
 
-client = mqtt.Client(client_id="")
+client = mqtt.Client(client_id="") # client_id는 생략 가능
 client.on_connect = on_connect
-client.on_message = on_message  
+client.on_message = on_message  # subscribe 메세지가 들어오면 작동하는 Method
 
 ca_file, cert_file, key_file = './certification/ca.pem', './certification/cert.pem', './certification/cert-key.pem' 
 
